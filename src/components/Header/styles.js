9b5fb0e30ce1113a68895262bfc618a4379525c2
Joao Vinicius <joaovvs@gmail.com>
2023-11-08 16:58:05 -0300

    done

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,17 +1,33 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
 export const Container = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin: 0 24px;
-  padding: 16px 0;
+  margin: 0 2.4rem;
+  padding: 1.6rem 0;
 
   border-bottom-width: 1px;
   border-bottom-style: solid;
   border-bottom-color: ${({ theme }) => theme.COLORS.GRAY_500};
 
   > h1 {
-    font-size: 32px;
+    font-size: 3.2rem;
+  }
+`;
+
+export const Menu = styled.button`
+  background: none;
+  border: none;
+  display: none;
+
+  > svg{
+    font-size: 4rem;
+    color: ${({theme})=> theme.COLORS.BLUE_200};
+  }
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.MD}){
+    display: block;
   }
 `;
